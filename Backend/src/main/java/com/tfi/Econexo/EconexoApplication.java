26e8887573f51ec8c50c7cf8cf5b7762b90b6fc2
Gabriela Camacho <412016@tecnicatura.frc.utn.edu.ar>
2026-05-14 17:19:48 -0300

    SCRUM-2 : Inicializando proyecto con Spring Boot 3.x

---
+++ Backend/src/main/java/com/tfi/Econexo/EconexoApplication.java
@@ -0,0 +1,13 @@
+package com.tfi.Econexo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EconexoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EconexoApplication.class, args);
+	}
+
+}
---
+++ Backend/src/test/java/com/tfi/Econexo/EconexoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tfi.Econexo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EconexoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
