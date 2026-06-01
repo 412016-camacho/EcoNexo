e8787f280624421ffb28767d9c48241e48e9dd69
Gabriela Camacho <412016@tecnicatura.frc.utn.edu.ar>
2026-05-31 21:55:22 -0300

    SCRUM-50 agregando componentes de navbar y footer

---
+++ Frontend/econexo/src/app/shared/components/navbar/navbar.component.ts
@@ -0,0 +1,26 @@
+import {Component, inject, Input} from '@angular/core';
+import {AuthService} from '../../../core/services/auth.service';
+import {Router} from '@angular/router';
+
+@Component({
+  selector: 'app-navbar',
+  imports: [],
+  templateUrl: './navbar.component.html',
+  styleUrl: './navbar.component.css'
+})
+export class NavbarComponent {
+  private authService = inject(AuthService);
+  private router = inject(Router);
+
+  @Input() userName: string = '';
+
+  logout() {
+    this.authService.logout().subscribe({
+      next: () => this.router.navigate(['/login']),
+      error: (err) => {
+        console.error('Error al cerrar sesión', err);
+        this.router.navigate(['/login']);
+      }
+    });
+  }
+}
