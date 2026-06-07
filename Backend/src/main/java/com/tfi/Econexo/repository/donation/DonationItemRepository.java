c01f0ae9ec3fc154c1da9a67860bbfb7c388be14
Gabriela Camacho <412016@tecnicatura.frc.utn.edu.ar>
2026-06-07 07:38:35 -0300

    SCRUM-84 Método findByDonation_StatusOrderByExpirationDateAsc agregado en DonationItemRepository

--- Backend/src/main/java/com/tfi/Econexo/repository/donation/DonationItemRepository.java
+++ Backend/src/main/java/com/tfi/Econexo/repository/donation/DonationItemRepository.java
@@ -1,9 +1,14 @@
 package com.tfi.Econexo.repository.donation;
 
 import com.tfi.Econexo.model.donation.DonationItem;
+import com.tfi.Econexo.model.enums.DonationStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface DonationItemRepository extends JpaRepository<DonationItem, Long> {
+
+    List<DonationItem> findByDonation_StatusOrderByExpirationDateAsc(DonationStatus status);
 }
