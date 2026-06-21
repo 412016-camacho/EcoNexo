3d0226213d3d2150af493c0385e9480f744a47fe
Gabriela Camacho <412016@tecnicatura.frc.utn.edu.ar>
2026-06-21 13:58:16 -0300

    SCRUM-127 Adapntando la interfaz de usuario a estados ASSIGNED e IN_TRANSIT en el mapa

--- Frontend/econexo/src/app/features/dashboard/components/available-trips/available-trips.component.ts
+++ Frontend/econexo/src/app/features/dashboard/components/available-trips/available-trips.component.ts
@@ -9,12 +9,16 @@ import {
   AcceptTripModalComponent
 } from '../accept-trip-modal/accept-trip-modal.component';
 import {Router} from '@angular/router';
+import {NavbarComponent} from '../../../../shared/components/navbar/navbar.component';
+import {FooterComponent} from '../../../../shared/components/footer/footer.component';
 
 @Component({
   selector: 'app-available-trips',
   imports: [
     DatePipe,
-    AcceptTripModalComponent
+    AcceptTripModalComponent,
+    NavbarComponent,
+    FooterComponent
   ],
   templateUrl: './available-trips.component.html',
   styleUrl: './available-trips.component.css'
