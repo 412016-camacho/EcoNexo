815ad9835c96f9a74dea6df685888d4b76a873ea
Gabriela Camacho <412016@tecnicatura.frc.utn.edu.ar>
2026-07-02 11:30:34 -0300

    SCRUM-158 Creación de NgoReception component

--- Frontend/econexo/src/app/routes/app.routes.ts
+++ Frontend/econexo/src/app/routes/app.routes.ts
@@ -14,6 +14,7 @@ import {ResetPasswordComponent} from '../features/auth/login/reset-password/rese
 import {AvailableTripsComponent} from '../features/dashboard/components/available-trips/available-trips.component';
 import {ActiveTripComponent} from '../features/dashboard/components/active-trip/active-trip.component';
 import {DonationsDetailComponent} from '../shared/components/donations-detail/donations-detail.component';
+import {NgoReceptionComponent} from '../features/organizations/ngo-reception/ngo-reception.component';
 
 export const routes: Routes = [
   { path: 'login', component: LoginComponent },
@@ -31,6 +32,7 @@ export const routes: Routes = [
   { path: 'dashboard/driver/available-trips', component: AvailableTripsComponent },
   { path: 'dashboard/trips/:id', component: ActiveTripComponent },
   { path: 'donations/form', component: DonationFormComponent },
+  { path: 'ngo/reception/:id', component: NgoReceptionComponent },
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: '**', redirectTo: 'login' }
 ];
