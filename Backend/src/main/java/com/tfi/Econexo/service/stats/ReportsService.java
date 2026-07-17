a00b185af4173dc16b30805edd41012cd1138832
Gabriela Camacho <412016@tecnicatura.frc.utn.edu.ar>
2026-07-17 17:15:04 -0300

    SCRUM-184 Estadísticas para el administrador

--- Backend/src/main/java/com/tfi/Econexo/service/stats/ReportsService.java
+++ Backend/src/main/java/com/tfi/Econexo/service/stats/ReportsService.java
@@ -1,9 +1,10 @@
 package com.tfi.Econexo.service.stats;
 
-import com.tfi.Econexo.dto.stats.AdminStatsDTO;
 import com.tfi.Econexo.dto.stats.DonorStatsDTO;
 import com.tfi.Econexo.dto.stats.DriverStatsDTO;
 import com.tfi.Econexo.dto.stats.NgoStatsDTO;
+
+import java.util.Map;
 
 public interface ReportsService {
 
@@ -11,6 +12,6 @@ public interface ReportsService {
     NgoStatsDTO getNgoStats(String email);
     DonorStatsDTO getDonorStats(String email);
     DriverStatsDTO getDriverStats(String email);
-    AdminStatsDTO getAdminStats(String email);
+    Map<String, Object> getAdminStats(String email);
 
 }
