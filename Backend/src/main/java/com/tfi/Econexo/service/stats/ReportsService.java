636d0fdd23524ab520371618c69127a5b9ea718e
Gabriela Camacho <412016@tecnicatura.frc.utn.edu.ar>
2026-08-22 09:06:50 -0300

    Upgrade en las estadísticas del conductor, complementando los reportes iniciales.

--- Backend/src/main/java/com/tfi/Econexo/service/stats/ReportsService.java
+++ Backend/src/main/java/com/tfi/Econexo/service/stats/ReportsService.java
@@ -1,7 +1,7 @@
 package com.tfi.Econexo.service.stats;
 
 import com.tfi.Econexo.dto.stats.donor.DonorStatsDTO;
-import com.tfi.Econexo.dto.stats.DriverStatsDTO;
+import com.tfi.Econexo.dto.stats.driver.DriverStatsDTO;
 import com.tfi.Econexo.dto.stats.NgoStatsDTO;
 
 import java.time.LocalDate;
@@ -12,7 +12,7 @@ public interface ReportsService {
     Object getStatsByRole(String role, String username, LocalDate startDate, LocalDate endDate);
     NgoStatsDTO getNgoStats(String email);
     DonorStatsDTO getDonorStats(String email, LocalDate startDate, LocalDate endDate);
-    DriverStatsDTO getDriverStats(String email);
+    DriverStatsDTO getDriverStats(String email, LocalDate startDate, LocalDate endDate);
     Map<String, Object> getAdminStats(String email, String role, LocalDate startDate, LocalDate endDate);
 
 }
